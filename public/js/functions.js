8d7e0ea24eb7344b6f76610027ee801112d53432
Alan Languirand <alan@13protons.com>
2013-11-15 15:57:06 -0500

    Realitem index page vote updating

--- public/js/functions.js
+++ public/js/functions.js
@@ -4,47 +4,23 @@ $(function(){
     var alts = ['resource', 'home'];
     var preloads = {
         'home': function(){
-            fbRefs = []
+            var fbRefs = []
             for(i in resources){
                 //console.log(resources[i].update);
                 fbRefs[i] = new Firebase(resources[i].update);
                 fbRefs[i].on('value', function(snapshot){
-                    console.log(this);
+                    console.log("changing", this.title);
                     if(snapshot.val() !== null) {
                         var targetID = this.id;
-                        $('#' + targetID).find('.votes').html(this.voteups + "/" + this.votes);
+                        
+                        $('#' + targetID).find('.votes').html(snapshot.val().up + "/" + snapshot.val().total);
                     }
                 }, function(){}, resources[i]);
             }
         }, 
         'resource': function(){
-            //load the page's data
-            $.getJSON(updateUrl, function(d){
-                    updatePage(d);
-            });
-            
-            $("#vote .button.ok, #vote .button.remove").click(function(event){
-                event.preventDefault();
-                if(!$(this).hasClass('active')){
-                    $("#vote .button").removeClass('active');
-                    $(this).addClass('active');
-                    $('#usage_graph').html("updating...");
-                    $.getJSON($(this).attr('href'), function(d){
-                        updatePage(d);
-                    });
-                }
-            });
-            
-            function updatePage(d){
-                $('#usage_graph').removeClass().addClass("bar_" + d.percent).html(d.percent + "%");
-                $("#vote_meta .assertion").html(d.status.assertion);
-                $("#vote_meta .punctuation").html(d.status.punctuation);
-                $("#vote_meta .votes").html(d.votes);
-                
-                $("#vote .button.ok, #vote .button.remove").removeClass("active");
-                if(d.your_vote === true){ $("#vote .button.ok").addClass("active");}
-                if(d.your_vote === false){ $("#vote .button.remove").addClass("active");}
-            }
+           
+           
             
         },
         '*': function(){
